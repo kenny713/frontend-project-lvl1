562a8ac29e9a562e58daaa4140d86e84f03b070d
kenny713 <lucker713@bk.ru>
2021-11-19 17:00:34 +0500

    added make lint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { name } from '../src/cli.js';
+import name from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 const userName = name();
 console.log(`Hello, ${userName}!`);
