8331ba1811232c07a848c8565192905dec1a3cb4
kenny713 <lucker713@bk.ru>
2021-12-06 14:17:35 +0500

    maked even-game

---
+++ src/games/even-game.js
@@ -0,0 +1,16 @@
+import getRandomNumber from '../common.js';
+import runGame from '../index.js';
+
+const isEven = (number) => number % 2 === 0;
+
+const description = 'Answer "yes" if given number os even. Otherwise answer "no".';
+const getGameData = () => {
+	const number = getRandomNumber(0, 25);
+	const question) = `${number}`;
+	const answer = isEven(number) ? 'yes' : 'no';
+	return [question, String(answer)];
+};
+
+const brainEven = () => runGame(description, getGameData);
+
+export default brainEven;
