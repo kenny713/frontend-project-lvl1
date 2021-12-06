0db9904e6b5b08b7288a2fdbfaab6f8d07c42596
kenny713 <lucker713@bk.ru>
2021-12-06 14:27:41 +0500

    fixed brain-even, brain-games and cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = name();
-console.log(`Hello, ${userName}!`);
+import requestName from '../src/cli.js';
+
+requestName();
