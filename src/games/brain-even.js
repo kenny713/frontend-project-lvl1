d43d443ce11cc3cd57b68959c66dc8bcce0cb957
kenny713 <lucker713@bk.ru>
2021-12-06 14:54:03 +0500

    brain-even done

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,10 +3,10 @@ import runGame from '../index.js';
 
 const isEven = (number) => number % 2 === 0;
 
-const description = 'Answer "yes" if given number os even. Otherwise answer "no".';
+const description = 'Answer "yes" if given number is even. Otherwise answer "no".';
 const getGameData = () => {
 	const number = getRandomNumber(0, 25);
-	const question) = `${number}`;
+	const question = `${number}`;
 	const answer = isEven(number) ? 'yes' : 'no';
 	return [question, String(answer)];
 };
