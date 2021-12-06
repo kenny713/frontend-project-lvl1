192904321425aaee417ba9acff84ee9340fa6800
kenny713 <lucker713@bk.ru>
2021-12-06 18:54:24 +0500

    fixed linter error

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,10 +5,10 @@ const isEven = (number) => number % 2 === 0;
 
 const description = 'Answer "yes" if given number is even. Otherwise answer "no".';
 const getGameData = () => {
-	const number = getRandomNumber(0, 25);
-	const question = `${number}`;
-	const answer = isEven(number) ? 'yes' : 'no';
-	return [question, String(answer)];
+  const number = getRandomNumber(0, 25);
+  const question = `${number}`;
+  const answer = isEven(number) ? 'yes' : 'no';
+  return [question, String(answer)];
 };
 
 const brainEven = () => runGame(description, getGameData);
